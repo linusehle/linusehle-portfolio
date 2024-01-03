b66e8d623515c65a59d245244f91b10824ddfff8
linusehle <linusfinn.ehle@gmail.com>
2024-01-03 15:16:55 +0100

    !feat: Inital commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,49 @@
 <template>
-    Home
+    <div class="bg-[#030712] text-white py-16">
+        <div class="mx-auto max-w-7xl">
+            <div class="mx-auto grid px-8">
+                <h2 class="text-base font-semibold leading-7">Welcome to my Portfolio Website!</h2>
+                <p class="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">Linus Ehle</p>
+                <p class="mt-4 text-base leading-7 text-zinc-400">
+                    I'm a Developer based in Stuttgart, Germany.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="bg-[#030712] text-white">
+        <div class="mx-auto max-w-7xl">
+            <div class="grid px-8 max-w-2xl">
+                <h2 class="text-lg font-semibold leading-7" id="about">About me</h2>
+                <p class="mt-8 text-base leading-7">
+                    Hi, I'm Linus Ehle, a 16 year old Developer, based in Stuttgart, Germany.
+                    I'm a passionate developer on a constant quest for new challenges and
+                    innovative solutions. 
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="bg-[#030712] text-white pt-8">
+        <div class="mx-auto max-w-7xl">
+            <div class="grid px-8 max-w-2xl">
+                <h2 class="text-lg font-semibold leading-7" id="portfolio">Portfolio</h2>
+                <p class="mt-8 text-base leading-7">
+                    Here is my tech stack.
+                </p>
+            </div>
+            <div class="grid px-8 max-w-7xl mx-auto">
+                <div>
+                    <img class="h-20" src="../assets/480px-HTML5_Badge.svg.png" />
+                </div>
+                <div>
+                    <img class="h-20" src="../assets/480px-CSS3_logo.png" />
+                </div>
+                <div>
+                    <img class="h-20" src="../assets/480px-Unofficial_JavaScript_logo_2.svg.png" />
+                </div>
+                <div>
+                    <img class="h-20" src="../assets/Git-logo-orange.svg" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
