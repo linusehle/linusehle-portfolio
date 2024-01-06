d0613a0940814ec01a80cb18ec2d4113ce14a9fb
linusehle <linusfinn.ehle@gmail.com>
2024-01-06 21:53:29 +0100

    feat: added link section

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,78 +1,135 @@
 <template>
-    <div class="bg-[#1e1e2e] text-white">
-        <div class="mx-auto max-w-7xl pl-4">
-            <div>
-                <h2 class="text-2xl font-semibold tracking-tight">Linus Ehle</h2>
-                <p class="text-zinc-400">Person struggling with coffee addiction</p>
-            </div>
-            <div class="grid mt-6 max-w-2xl">
-                <h2 class="text-xl font-semibold tracking-tight text-[#ABE9B3]">> About me</h2>
-                <p class="text-zinc-400">I' am 16 year old student based in Stuttgart, Germany.
-                I love to code and build applications and as well, ususal for a developer, I'm 
-                addicted to coffee. 
-                </p>
-                <div class="flex space-x-4 mt-4 items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="text-zinc-400 h-5">
-                        <path fill="currentColor" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0
-                        192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1
-                        15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
-                    </svg>
-                    <p class="text-zinc-400 font-semibold">Stuttgart, Germany</p>
+    <body class="bg-[#1e1e2e]">
+            <div class="bg-[#1e1e2e] text-white flex justify-center mx-auto max-w-6xl pt-8">
+                <div class="mx-auto pl-4">
+                    <div>
+                        <h2 class="text-2xl font-semibold tracking-tight">Linus Ehle</h2>
+                        <p class="text-zinc-400">Person struggling with coffee addiction</p>
+                    </div>
+                    <div class="grid mt-6 max-w-xl">
+                        <h2 class="text-xl font-semibold tracking-tight text-[#ABE9B3]">> About me</h2>
+                        <p class="text-zinc-400">I' am 16 year old student based in Stuttgart, Germany.
+                        I love to code and build applications and as well, ususal for a developer, I'm 
+                        addicted to coffee. 
+                        </p>
+                    <div class="flex space-x-4 mt-4 items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="text-zinc-400 h-5">
+                            <path fill="currentColor" d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0
+                            192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1
+                            15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/>
+                        </svg>
+                        <p class="text-zinc-400 font-semibold">Stuttgart, Germany</p>
+                    </div>
+                </div>
+                <div class="grid mt-6 max-w-2xl">
+                    <h2 class="text-xl font-semibold tracking-tight text-[#ABE9B3]">> Skills</h2>
+                    <ul class="mt-6 space-y-4">
+                        <li class="flex space-x-4 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="text-zinc-400 h-5">
+                                <path fill="currentColor" d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1
+                                49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5
+                                53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z"/>
+                            </svg>
+                            <span class="text-zinc-400 font-semibold">Html5</span>
+                        </li>
+                        <li class="flex space-x-4 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="text-zinc-400 h-5">
+                                <path fill="currentColor" d="M0 32l34.9 395.8L192 480l157.1-52.2L384 32H0zm313.1 80l-4.8 47.3L193
+                                208.6l-.3 .1h111.5l-12.8 146.6-98.2 28.7-98.8-29.2-6.4-73.9h48.9l3.2 38.3 52.6
+                                13.3 54.7-15.4 3.7-61.6-166.3-.5v-.1l-.2 .1-3.6-46.3L193.1 162l6.5-2.7H76.7L70.9
+                                112h242.2z"/>
+                            </svg>
+                            <span class="text-zinc-400 font-semibold">Css3</span>
+                        </li>
+                        <li class="flex space-x-4 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="text-zinc-400 h-5">
+                                <path fill="currentColor" d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5
+                                39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7
+                                256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 
+                                12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3
+                                0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 
+                                0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
+                            </svg>
+                            <span class="text-zinc-400 font-semibold">TailwindCss</span>
+                        </li>
+                        <li class="flex space-x-4 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-zinc-400 h-5">
+                                <path fill="currentColor" d="M0 32v448h448V32H0zm243.8 349.4c0 43.6-25.6 63.5-62.9 63.5-33.7 
+                                0-53.2-17.4-63.2-38.5l34.3-20.7c6.6 11.7 12.6 21.6 27.1 21.6 13.8 0 22.6-5.4
+                                22.6-26.5V237.7h42.1v143.7zm99.6 63.5c-39.1 0-64.4-18.6-76.7-43l34.3-19.8c9
+                                14.7 20.8 25.6 41.5 25.6 17.4 0 28.6-8.7 28.6-20.8 
+                                0-14.4-11.4-19.5-30.7-28l-10.5-4.5c-30.4-12.9-50.5-29.2-50.5-63.5 
+                                0-31.6 24.1-55.6 61.6-55.6 26.8 0 46 9.3 59.8 33.7L368 290c-7.2-12.9-15-18-27.1-18-12.3 
+                                0-20.1 7.8-20.1 18 0 12.6 7.8 17.7 25.9 25.6l10.5 4.5c35.8 15.3 55.9 31 55.9 66.2 
+                                0 37.8-29.8 58.6-69.7 58.6z"/>
+                            </svg>
+                            <span class="text-zinc-400 font-semibold">Javascript</span>
+                        </li>
+                        <li class="flex space-x-4 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-zinc-400 h-5">
+                                <path fill="currentColor" d="M356.9 64.3H280l-56 88.6-48-88.6H0L224 448 448 64.3h-91.1zm-301.2 
+                                32h53.8L224 294.5 338.4 96.3h53.8L224 384.5 55.7 96.3z"/>
+                            </svg>
+                            <span class="text-zinc-400 font-semibold">VueJS</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="grid mt-6 max-w-2xl">
+                    <h2 class="text-xl font-semibold tracking-tight text-[#ABE9B3]">> Links</h2> 
+                    <ul class="mt-6 space-y-4">
+                        <li class="flex space-x-4 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="text-zinc-400 h-5">
+                                <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6
+                                3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5
+                                .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8
+                                8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4
+                                0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6
+                                27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 
+                                23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0
+                                0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5
+                                9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252
+                                496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 
+                                1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 
+                                .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 
+                                5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
+                            </svg>
+                            <a href="https://github.com/linusehle" target="_blank">
+                                <span class="text-zinc-400 font-semibold">GitHub</span>
+                            </a>
+                        </li>
+                        <li class="flex space-x-4 items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-zinc-400 h-5">
+                                <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5
+                                339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7
+                                74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 
+                                26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 
+                                27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8
+                                1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7
+                                36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2
+                                34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 
+                                11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 
+                                9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 
+                                42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
+                            </svg>
+                            <a href="" target="_blank">
+                                <span class="text-zinc-400 font-semibold">Instagram</span>
+                            </a>
+                        </li>
+                    </ul>   
                 </div>
             </div>
-            <div class="grid mt-6 max-w-2xl">
-                <h2 class="text-xl font-semibold tracking-tight text-[#ABE9B3]">> Skills</h2>
-                <ul class="mt-6 space-y-4">
-                    <li class="flex space-x-4 items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="text-zinc-400 h-5">
-                            <path fill="currentColor" d="M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1
-                            49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5
-                            53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z"/>
-                        </svg>
-                        <span class="text-zinc-400 font-semibold">Html5</span>
-                    </li>
-                    <li class="flex space-x-4 items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="text-zinc-400 h-5">
-                            <path fill="currentColor" d="M0 32l34.9 395.8L192 480l157.1-52.2L384 32H0zm313.1 80l-4.8 47.3L193
-                            208.6l-.3 .1h111.5l-12.8 146.6-98.2 28.7-98.8-29.2-6.4-73.9h48.9l3.2 38.3 52.6
-                            13.3 54.7-15.4 3.7-61.6-166.3-.5v-.1l-.2 .1-3.6-46.3L193.1 162l6.5-2.7H76.7L70.9
-                            112h242.2z"/>
-                        </svg>
-                        <span class="text-zinc-400 font-semibold">Css3</span>
-                    </li>
-                    <li class="flex space-x-4 items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="text-zinc-400 h-5">
-                            <path fill="currentColor" d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5
-                            39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7
-                            256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 
-                            12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3
-                            0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 
-                            0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/>
-                        </svg>
-                        <span class="text-zinc-400 font-semibold">TailwindCss</span>
-                    </li>
-                    <li class="flex space-x-4 items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-zinc-400 h-5">
-                            <path fill="currentColor" d="M0 32v448h448V32H0zm243.8 349.4c0 43.6-25.6 63.5-62.9 63.5-33.7 
-                            0-53.2-17.4-63.2-38.5l34.3-20.7c6.6 11.7 12.6 21.6 27.1 21.6 13.8 0 22.6-5.4
-                            22.6-26.5V237.7h42.1v143.7zm99.6 63.5c-39.1 0-64.4-18.6-76.7-43l34.3-19.8c9
-                            14.7 20.8 25.6 41.5 25.6 17.4 0 28.6-8.7 28.6-20.8 
-                            0-14.4-11.4-19.5-30.7-28l-10.5-4.5c-30.4-12.9-50.5-29.2-50.5-63.5 
-                            0-31.6 24.1-55.6 61.6-55.6 26.8 0 46 9.3 59.8 33.7L368 290c-7.2-12.9-15-18-27.1-18-12.3 
-                            0-20.1 7.8-20.1 18 0 12.6 7.8 17.7 25.9 25.6l10.5 4.5c35.8 15.3 55.9 31 55.9 66.2 
-                            0 37.8-29.8 58.6-69.7 58.6z"/>
-                        </svg>
-                        <span class="text-zinc-400 font-semibold">Javascript</span>
-                    </li>
-                    <li class="flex space-x-4 items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-zinc-400 h-5">
-                            <path fill="currentColor" d="M356.9 64.3H280l-56 88.6-48-88.6H0L224 448 448 64.3h-91.1zm-301.2 
-                            32h53.8L224 294.5 338.4 96.3h53.8L224 384.5 55.7 96.3z"/>
-                        </svg>
-                        <span class="text-zinc-400 font-semibold">VueJS</span>
-                    </li>
-                </ul>
+            <div class="mx-auto">
+                <div class="text-center">
+                    <h2 class="text-2xl font-semibold tracking-tight">Projects</h2>
+                    <p class="text-zinc-400 font-semibold">Some of my best works</p>
+                </div>
+                <div class="grid">
+                    <div class="bg-[#1a1826] rounded-xl px-8">
+                        <h2 class="text-xl font-semibold tracking-tight text-[#ABE9B3]">LSS-Scripts</h2>
+                        <p class="text-zinc-400 font-semibold">These are Scripts to modifiy the game Lss</p>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+    </body>
 </template>
