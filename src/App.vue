60ac2f6a75fd23384ed1352f4d221dcf64c1efed
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2024-01-22 06:15:45 +0100

    feat: imported navbar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView} from 'vue-router';
+import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 </script>
 
 <template>
+  <Navbar />
   <router-view></router-view>
   <Footer />
 </template>
