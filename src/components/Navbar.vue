04b38afd9e3f342de61fad109fab1a0d776396b9
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2024-01-24 11:53:06 +0100

    feat: made navbar color change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <header class="md:flex md:justify-center md:items-center">
       <div class="flex items-center justify-between px-4 pb-4 py-3 sm:p-0">
         <div class="pt-4 pl-4 pb-4 md:hidden">
-          <button @click="isOpen = !isOpen" type="button" class="block text-white focus:text-white focus:outline-none">
+          <button @click="isOpen = !isOpen" type="button" class="block text-black focus:text-black focus:outline-none">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
               <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
@@ -11,33 +11,18 @@
         </div>
       </div>
       <nav :class="isOpen ? 'flex' : 'hidden'" class="md:flex">
-        <ul class="flex text-center w-full flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-8 md:justify-center md:items-center max-[1000px]:pt-8 pb-4">
+        <ul class="flex text-center w-full flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-8 md:justify-center md:items-center pt-8 pb-4">
             <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#6c63ff]': $route.path === '/'}" to="/">Home</router-link>
+              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/'}" to="/">Home</router-link>
             </li>
             <li>
-              <span class="text-lg font-semibold leading-6 hover:text-[#00a8f0] cursor-pointer transition" v-if="$route.path === '/en'"  v-on:click="about_scroll">About us</span>
-              <span class="text-lg font-semibold leading-6 hover:text-[#00a8f0] cursor-pointer transition" v-else v-on:click="about_scroll">Über uns</span>
+              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/about'}" to="/">About</router-link>
             </li>
             <li>
-              <span class="text-lg font-semibold left-6 hover:text-[#00a8f0] cursor-pointer transition" v-if="$route.path === '/en'" v-on:click="services_scroll">Services</span>
-              <span class="text-lg font-semibold left-6 hover:text-[#00a8f0] cursor-pointer transition" v-else v-on:click="services_scroll">Leistungen</span>
+              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/projects'}" to="/">Projects</router-link>
             </li>
             <li>
-              <span class="text-lg font-semibold leading-6 hover:text-[#00a8f0] cursor-pointer transition" v-on:click="portfolio_scroll">Portfolio</span>
-            </li>
-            <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" v-if="$route.path === '/en'" :class="{'text-[#00a8f0]': $route.path === '/contact'}" to="/contact">Contact</router-link>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" v-else :class="{'text-[#00a8f0]': $route.path === '/contact'}" to="/contact">Kontakt</router-link>
-            </li>
-            <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/impressum'}" to="/impressum">Impressum</router-link>
-            </li>
-            <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/'}" to="/">DE</router-link>
-            </li>
-            <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#00a8f0]" :class="{'text-[#00a8f0]': $route.path === '/en'}" to="/en">EN</router-link>
+              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/contact'}" to="/">Contact</router-link>
             </li>
           </ul>
       </nav>
