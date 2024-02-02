5dce6b0f720a26035fbfd0644e442d864a542e4c
linusehle <linusfinn.ehle@gmail.com>
2024-02-02 22:28:35 +0100

    feat: worked on the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,24 @@
 <template>
-    <div class="bg-[#1a1826] text-white">
-        <div class="mx-auto max-w-7xl">
-            
+    <div class="bg-[#331c52] w-full flex justify-center">
+        <div class="pt-8">
+            <span class="text-white font-bold text-4xl italic">Linus</span>
+            <div class="flex">
+                <div>
+                    <a href="" target="_blank">
+                        <span class="text-white">Twitter</span>
+                    </a>
+                </div>
+                <div>
+                    <a href="" target="_blank">
+                        <span class="text-white"></span>
+                    </a>
+                </div>
+                <div>
+                    <a href="" target="_blank">
+                        <span class="text-white">Twitter</span>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
