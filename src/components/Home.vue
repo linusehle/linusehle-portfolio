8e91b683a3a65d10560a2013303afd4685c98d37
linusehle <linusfinn.ehle@gmail.com>
2024-02-02 22:28:51 +0100

    feat: worked on the main page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,126 +1,71 @@
 <template>
-    <div class="flex flex-col lg:flex-row">
-        <div class="pt-8">
-            <div class="grid space-y-4 px-4 max-w-lg mx-auto md:mx-[125px]">
-                <span class="font-bold leading-6 text-4xl">I'm Linus</span>
-                <span class="font-semibold leading-6">I'm a 16 year old student based in Stuttgart, Germany.
-                I love to code and do it as a daily routine, I build applications  and as well, usual for a
-                developer, I'm addicted to coffee.
-                </span>
-            </div>
-            <div class="mt-10 space-x-4 max-w-lg mx-auto px-4 md:mx-[125px]">
-                <router-link class="bg-[#6c63ff] rounded-full p-2 text-lg px-3 text-white font-semibold" to="/about">
-                    Learn more
-                </router-link>
-                <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff] cursor-pointer" to="/contact">
-                    Contact
-                    <span>→</span>
-                </router-link>
-            </div>
-            <div class="flex space-x-4 pl-4 pr-4 mt-12 max-w-lg mx-auto md:mx-[125px]">
-                <div>
-                    <a href="https://github.com/linusehle" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="h-8 hover:text-[#6c63ff]">
-                            <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6
-                            0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3
-                            4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 
-                            .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6
-                            2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0
-                            113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6
-                            17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 
-                            0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 
-                            21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 
-                            16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 
-                            23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 
-                            41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 
-                            5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 
-                            110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 
-                            12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 
-                            8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 
-                            1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 
-                            3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 
-                            35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 
-                            .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 
-                            1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
-                        </svg>
-                    </a>
+    <div class=" flex justify-center">
+        <div class="flex space-x-4 items-center justify-center pt-16">
+            <span class="uppercase text-2xl font-semibold">Made with</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="text-[#1373e6] h-8">
+                <path fill="currentColor" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 
+                300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 
+                268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0
+                41.5 17.2 81.2 47.6 109.5z"/>
+            </svg>
+        </div>
+    </div>
+    <div class=" flex justify-center pt-[225px]">
+        <div>
+            <span class="text-9xl font-bold">Welcome!</span>
+        </div>
+    </div>
+    <div class=" flex justify-center pt-[300px] cursor-pointer">
+        <div class="bg-[#1373e6] rounded-full text-white p-4" v-on:click="scroll_main">
+            <span class="text-xl font-bold">↓</span>
+        </div>
+    </div>
+    <div id="main" class="h-screen flex flex-col items-center justify-center">
+        <div class="flex items-center space-x-2">
+            <span class="text-4xl font-semibold">Hello there</span>
+            <img class="h-10" src="../assets/waving_emoijo.png" />
+            <span class="text-4xl font-semibold">I'm</span>
+        </div>
+        <div>
+            <span class="text-9xl font-bold text-[#1373e6]">Linus</span>
+        </div>
+        <div>
+            <span class="text-6xl font-bold">developer</span>
+        </div>
+        <div class="h-[250px] border-dashed border-4 border-[#1373e6] mt-[100px]"></div>
+    </div>
+    <div class="flex flex-col justify-center">
+        <div class="flex flex-col text-center">
+            <span class="text-4xl font-semibold">Explore my</span>
+            <span class="text-6xl font-bold text-[#1373e6]">Experience</span>
+        </div>
+        <div class="flex justify-center space-x-6">
+            <div class="border border-black p-6 rounded-3xl">
+                <div class="text-center">
+                    <span class="text-2xl font-semibold">Frontend Development</span>
                 </div>
-                <div>
-                    <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="h-8 hover:text-[#6c63ff]">
-                            <path fill="currentColor" d="M248 8C111.1 8 0 119.1 0 256s111.1 248 248 248 248-111.1 248-248S384.9 8 248 
-                            8zm100.7 364.9c-4.2 0-6.8-1.3-10.7-3.6-62.4-37.6-135-39.2-206.7-24.5-3.9 1-9 2.6-11.9 
-                            2.6-9.7 0-15.8-7.7-15.8-15.8 0-10.3 6.1-15.2 13.6-16.8 81.9-18.1 165.6-16.5 237 26.2 6.1 
-                            3.9 9.7 7.4 9.7 16.5s-7.1 15.4-15.2 15.4zm26.9-65.6c-5.2 0-8.7-2.3-12.3-4.2-62.5-37-155.7-51.9-238.6-29.4-4.8 
-                            1.3-7.4 2.6-11.9 2.6-10.7 0-19.4-8.7-19.4-19.4s5.2-17.8 15.5-20.7c27.8-7.8 56.2-13.6 97.8-13.6 64.9 0 127.6 
-                            16.1 177 45.5 8.1 4.8 11.3 11 11.3 19.7-.1 10.8-8.5 19.5-19.4 19.5zm31-76.2c-5.2 
-                            0-8.4-1.3-12.9-3.9-71.2-42.5-198.5-52.7-280.9-29.7-3.6 1-8.1 2.6-12.9 2.6-13.2 0-23.3-10.3-23.3-23.6 0-13.6 
-                            8.4-21.3 17.4-23.9 35.2-10.3 74.6-15.2 117.5-15.2 73 0 149.5 15.2 205.4 47.8 7.8 4.5 12.9 10.7 12.9 22.6 0 
-                            13.6-11 23.3-23.2 23.3z"/>
-                        </svg>
-                    </a>
-                </div>
-                <div>
-                    <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="h-8 hover:text-[#6c63ff]">
-                            <path fill="currentColor" d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 
-                            0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 
-                            0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 
-                            0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 
-                            176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 
-                            321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 
-                            1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 
-                            7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 
-                            391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 
-                            .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 
-                            219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 
-                            0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 
-                            311 447.5 337.6 417.9 337.6z"/>
-                        </svg>
-                    </a>
-                </div>
-                <div>
-                    <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="h-8 hover:text-[#6c63ff]">
-                            <path fill="currentColor" d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 
-                            288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 
-                            42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 
-                            47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 
-                            48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 
-                            213.5V175.2l142.7 81.2-142.7 81.2z"/>
-                        </svg>
-                    </a>
-                </div>
-                <div>
-                    <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-8 hover:text-[#6c63ff]">
-                            <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 
-                            114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 
-                            0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 
-                            74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 
-                            26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 
-                            0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 
-                            34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 
-                            0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 
-                            2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 
-                            9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 
-                            9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/>
-                        </svg>
-                    </a>
-                </div>
-                <div>
-                    <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-8 hover:text-[#6c63ff]">
-                            <path fill="currentColor" d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 
-                            0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 
-                            1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 
-                            13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 
-                            0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 
-                            21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z"/>
-                        </svg>
-                    </a>
+                <div class="grid grid-cols-2">
+                    <div class="flex flex-col">
+                        <span class="text-xl font-bold">Html</span>
+                        <span class="text-xl ">Experienced</span>
+                    </div>
+                    <div class="flex flex-col">
+                        <span class="text-xl font-bold">Css</span>
+                        <span class="text-xl">Experienced</span>
+                    </div>
                 </div>
             </div>
+            <div class="border border-black p-6 rounded-3xl">
+                <div class="text-center">
+                    <span class="text-2xl font-semibold">Frontend Development</span>
+                </div>
+            </div>            
         </div>
     </div>
 </template>
+
+<script setup>
+function scroll_main (event) {
+    document.getElementById("main").scrollIntoView({behavior: 'smooth'})
+}
+</script>
