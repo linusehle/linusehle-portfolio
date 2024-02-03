5419c2c9b11a9343c74f46b5d53defab836810d3
linusehle <linusfinn.ehle@gmail.com>
2024-02-03 12:35:26 +0100

    feat: worked on the main page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -173,7 +173,27 @@
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="flex justify-center">
+            <div class="h-[250px] w-0 border-dashed border-4 border-[#1373e6] mt-[100px]"></div>
         </div>     
+    </div>
+    <div class="flex justify-center text-center pt-6">
+        <span class="text-7xl font-bold">Thanks you!<br>for looking my journey.</span>
+    </div>
+    <div class="flex space-x-4 items-center justify-center pt-8">
+        <span class="uppercase text-2xl font-semibold">Made with</span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="text-[#1373e6] h-8">
+            <path fill="currentColor" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 
+            300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 
+            268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0
+            41.5 17.2 81.2 47.6 109.5z"/>
+        </svg>
+    </div>
+    <div class="flex justify-center pt-12">
+        <div class="text-center">
+            <span class="font-semibold text-2xl">If you have a project, trust me. I can make it happen. <br>Reach me out.</span>
+        </div>
     </div>
 </template>
 
