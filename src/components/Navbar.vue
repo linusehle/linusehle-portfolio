725b6adf133550479b576b74033a943197c035eb
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2024-02-12 00:31:38 +0100

    feat: changed hamburger menu in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
-    <header class="md:flex md:justify-center md:items-center">
-      <div class="flex items-center justify-between py-3 sm:p-0">
-        <div class="pt-4 px-4 pb-4 sm:hidden">
+    <header class="fixed  md:flex md:items-center">
+      <div class="flex items-center py-3 sm:p-0">
+        <div class="pt-4 px-4 pb-4">
           <button @click="isOpen = !isOpen" type="button" class="block text-black focus:text-black focus:outline-none">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
@@ -10,10 +10,10 @@
           </button>
         </div>
       </div>
-      <nav :class="isOpen ? 'flex' : 'hidden'" class="sm:flex">
-        <ul class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-8 justify-start sm:items-center pt-8 pb-4">
+      <nav :class="isOpen ? 'flex' : 'hidden'" class="">
+        <ul class="flex flex-col space-y-4 pt-8 pb-4">
             <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/'}" to="/">Home</router-link>
+              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#5953ce]': $route.path === '/'}" to="/">Home</router-link>
             </li>
             <li>
               <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/about'}" to="/">About</router-link>
