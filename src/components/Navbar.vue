657247ae81dc11884e5426ca1730002b3bd4d8da
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2024-02-12 21:36:47 +0100

    feat: improved position of hamburger menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-    <header class="fixed  md:flex md:items-center">
+    <header class="fixed md:flex md:items-center">
       <div class="flex items-center py-3 sm:p-0">
         <div class="pt-4 px-4 pb-4">
-          <button @click="isOpen = !isOpen" type="button" class="block text-black focus:text-black focus:outline-none">
+          <button @click="isOpen = !isOpen" type="button" class="block text-white focus:text-white focus:outline-none">
             <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
               <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
               <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
@@ -13,16 +13,16 @@
       <nav :class="isOpen ? 'flex' : 'hidden'" class="">
         <ul class="flex flex-col space-y-4 pt-8 pb-4">
             <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#5953ce]': $route.path === '/'}" to="/">Home</router-link>
+              <router-link class="text-white text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-white': $route.path === '/'}" to="/">Home</router-link>
             </li>
             <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/about'}" to="/">About</router-link>
+              <router-link class="text-white text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-white': $route.path === '/about'}" to="/">About</router-link>
             </li>
             <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/projects'}" to="/">Projects</router-link>
+              <router-link class="text-white text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-white': $route.path === '/projects'}" to="/">Projects</router-link>
             </li>
             <li>
-              <router-link class="text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-[#6c63ff]': $route.path === '/contact'}" to="/">Contact</router-link>
+              <router-link class="text-white text-lg font-semibold leading-6 hover:text-[#6c63ff]" :class="{'text-white': $route.path === '/contact'}" to="/">Contact</router-link>
             </li>
           </ul>
       </nav>
