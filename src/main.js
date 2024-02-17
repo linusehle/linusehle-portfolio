8b009714989e896c07b4fe8461751c08daa6b3dc
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2024-02-17 07:50:38 +0100

    feat: added vue-typed.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import VueTypedJs from 'vue-typed-js'
 
 const app = createApp(App).use(router).mount("#app")
