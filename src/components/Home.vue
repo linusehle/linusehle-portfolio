e2c0794a6438e725c26f99423282e2a6d34e8b3b
linusehle <linusfinn.ehle@gmail.com>
2024-02-17 18:36:33 +0100

    feat: added social media hover effects

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,7 +17,7 @@
             <div class="flex space-x-6 text-white mt-6">
                 <div>
                     <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="text-white h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="text-white h-6 hover:text-[#9f55ff]">
                             <path fill="currentColor" d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 
                             50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 
                             8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 
@@ -28,7 +28,7 @@
                 </div>
                 <div>
                     <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-white h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-white h-6 hover:text-[#9f55ff]">
                             <path fill="currentColor" d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 
                             0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 
                             37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/>
@@ -37,7 +37,7 @@
                 </div>
                 <div>
                     <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="text-white h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="text-white h-6 hover:text-[#9f55ff]">
                             <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 
                             1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 
                             2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 
@@ -55,7 +55,7 @@
                 </div>
                 <div>
                     <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-white h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-white h-6 hover:text-[#9f55ff]">
                             <path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 
                             255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 
                             33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 
@@ -69,7 +69,7 @@
                 </div>
                 <div>
                     <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="text-white h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="text-white h-6 hover:text-[#9f55ff]">
                             <path fill="currentColor" d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 
                             0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 
                             1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 
