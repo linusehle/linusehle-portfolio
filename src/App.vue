e6e48ee2915145e2b8e31041ead03fe20629114c
linusehle <linusfinn.ehle@gmail.com>
2024-02-22 19:10:10 +0100

    feat: imported Test.vue

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import { RouterView} from 'vue-router';
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
+import Test from './components/Test.vue'
 </script>
 
 <template>
-  <Navbar /> 
+  <Test />
+  <Navbar />
   <router-view></router-view>
   <Footer />
 </template>
