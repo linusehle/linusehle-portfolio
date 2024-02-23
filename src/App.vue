63c19d1e297951325bce834d109d4b7c06be80c7
linusehle <linusfinn.ehle@gmail.com>
2024-02-23 18:52:59 +0100

    feat: renamed Test.vue import and usage to Cursor.vue import and usage

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
 import { RouterView} from 'vue-router';
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
-import Test from './components/Test.vue'
+import Cursor from './components/Cursor.vue'
 </script>
 
 <template>
-  <Test />
+  <Cursor />
   <Navbar />
   <router-view></router-view>
   <Footer />
