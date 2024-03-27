cb7c53c118d2b24ec7bda5b4c0656041a26f4662
linusehle <linusfinn.ehle@gmail.com>
2024-03-27 20:22:21 +0100

    feat: added hover effect on social media icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,10 +4,10 @@
             <div class="flex justify-center mb-12">
                 <span class="text-white font-bold font-mono text-3xl">Feel free to contact me!</span>
             </div>
-            <div class="flex justify-center space-x-6 text-white">
+            <div class="flex justify-center space-x-8 text-white">
                 <div>
                     <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="text-white h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="text-white h-6 hover:mb-2 transition-all duration-150">
                             <path fill="currentColor" d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 
                             50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 
                             8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 
@@ -18,7 +18,7 @@
                 </div>
                 <div>
                     <a href="" target="_blank">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-white h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="text-white h-6 hover:mb-2 transition-all duration-150">
                             <path fill="currentColor" d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 
                             0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 
                             37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/>
