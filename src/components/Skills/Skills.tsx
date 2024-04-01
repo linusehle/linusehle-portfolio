96bb7211a304929798bcb75a53c861634a0a31b9
linusehle <linusfinn.ehle@gmail.com>
2024-04-01 20:13:54 +0200

    feat: Inital commit

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,24 @@
+function Skills () {
+    return (
+        <div className="bg-[#120e16] h-screen">
+            <div className="flex flex-col py-12 px-4 md:px-[125px] lg:px-[300px]">
+                <div>
+                    <span className="font-medium text-base uppercase tracking-widest text-white">skills</span>
+                </div>
+                <div>
+                    <span className="font-bold font-mono text-3xl text-white">My Skills</span>
+                </div>
+                <div className="w-full sm:max-w-lg pt-4">
+                    <span className="font-semibold text-[1.65rem] text-white md:text-3xl">I like to build things with code and always learn new things.</span>
+                </div>
+                <div className="pt-16">
+                    <div>
+                        <span className="font-medium text-base uppercase tracking-widest text-gray-200">languages and tools</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills
