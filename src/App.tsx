a367d0d120e8609e3ad4da0a350432f1cfc311cc
linusehle <linusfinn.ehle@gmail.com>
2024-04-01 19:17:59 +0200

    feat: Importet About.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./Header.tsx"
+import About from "./components/About.tsx"
 import Footer from "./Footer.tsx";
 
 function App() {
   return (
     <>
       <Header />
+      <About />
       <Footer />
     </>
   );
