a7385cb9e348a48747a3e2d379780e2273c282c4
linusehle <linusfinn.ehle@gmail.com>
2024-04-01 20:09:26 +0200

    feat: Importet Skills and Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import Navbar from "./components/Navbar.tsx";
 import Header from "./Header.tsx"
 import About from "./components/About.tsx"
 import Footer from "./Footer.tsx";
+import Skills from "./components/Skills/Skills.tsx";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Header />
       <About />
+      <Skills />
       <Footer />
     </>
   );
