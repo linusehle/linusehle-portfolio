368158123c1fdd471f98b0cde67ea78ab827f0e8
linusehle <linusfinn.ehle@gmail.com>
2024-04-03 17:22:55 +0200

    feat: changed import of css file to import of sass/scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar.tsx";
-import Header from "./Header.tsx"
+import Header from "./components/Header/Header.tsx"
 import About from "./components/About.tsx"
 import Footer from "./Footer.tsx";
 import Skills from "./components/Skills/Skills.tsx";
