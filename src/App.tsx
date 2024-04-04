52b47eb8d4918695b913c5e105ce89c27ba962b0
linusehle <linusfinn.ehle@gmail.com>
2024-04-04 18:52:19 +0200

    feat: changed import settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar.tsx";
 import Header from "./components/Header/Header.tsx"
-import About from "./components/About.tsx"
+import About from "./components/About/About.tsx"
 import Footer from "./Footer.tsx";
 import Skills from "./components/Skills/Skills.tsx";
 
