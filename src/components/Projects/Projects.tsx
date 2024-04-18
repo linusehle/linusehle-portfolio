751e8c2fbe23c340352aef762dc8b1c8c4568941
linusehle <linusfinn.ehle@gmail.com>
2024-04-18 16:33:54 +0200

    feat: Inital commit

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,102 @@
+function Projects () {
+    return (
+        <div
+  className="bg-[#000000] text-white overflow-hidden py-14 pb-20"
+  id="portfolio"
+>
+  <div className="mx-auto px-4 py-5 sm:px-6 max-w-7xl">
+    <div className="max-w-3xl">
+      <h2 className="font-semibold text-[#01a7ee]">Our work</h2>
+      <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
+        Portfolio
+      </p>
+      <p className="mt-4 text-zinc-400">
+        Discover examples of our previous orders and gain an impression of the
+        impression of the quality you can expect from us.
+      </p>
+    </div>
+    <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16">
+      <a
+        className="group mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:flex items-center"
+        href=""
+      >
+        <div>
+          <div className="text-xl font-bold">M&amp;M Handwerk</div>
+          <div className="text-zinc-400 w-72">
+            <div className="mt-2">
+              Website for a craft business with contact form
+            </div>
+          </div>
+        </div>
+        <div className="lg:flex lg:space-x-6 space-x-0 overscroll-x-auto pl-16">
+          <div className="hidden lg:block">
+            <img
+              className="rounded-xl"
+              height={700}
+              width={225}
+              src="../assets/stonks.png"
+            />
+          </div>
+          <div className="hidden lg:block">
+            <img
+              className="rounded-xl"
+              height={700}
+              width={225}
+              src="../assets/stonks.png"
+            />
+          </div>
+          <div className="hidden lg:block">
+            <img
+              className="rounded-xl"
+              height={700}
+              width={225}
+              src="../assets/stonks.png"
+            />
+          </div>
+        </div>
+      </a>
+      <a
+        className="group mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:flex items-center"
+        href=""
+      >
+        <div>
+          <div className="text-xl font-bold">Ben's Burger</div>
+          <div className="text-zinc-400 w-72">
+            <div className="mt-2">Website for a burger company</div>
+          </div>
+        </div>
+        <div className="lg:flex lg:space-x-6 space-x-0 overscroll-x-auto pl-16">
+          <div className="hidden lg:block">
+            <img
+              className="rounded-xl"
+              height={700}
+              width={225}
+              src="../assets/stonks.png"
+            />
+          </div>
+          <div className="hidden lg:block">
+            <img
+              className="rounded-xl"
+              height={700}
+              width={225}
+              src="../assets/stonks.png"
+            />
+          </div>
+          <div className="hidden lg:block">
+            <img
+              className="rounded-xl"
+              height={700}
+              width={225}
+              src="../assets/stonks.png"
+            />
+          </div>
+        </div>
+      </a>
+    </div>
+  </div>
+</div>
+
+    );
+}
+
+export default Projects 
