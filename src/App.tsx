18655ccf15a09388bdf387031c199796f1cb07de
linusehle <linusfinn.ehle@gmail.com>
2024-04-18 17:18:49 +0200

    feat: importet Projects.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Header from "./components/Header/Header.tsx"
 import About from "./components/About/About.tsx"
+import Projects from "./components/Projects/Projects.tsx";
 import Footer from "./components/Footer/Footer.tsx";
 import Skills from "./components/Skills/Skills.tsx";
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <About />
       <Skills />
+      <Projects />
       <Footer />
     </>
   );
