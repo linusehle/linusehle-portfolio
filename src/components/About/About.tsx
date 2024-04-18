df50df151df4bb5c2b64e8443bd714dddbad331c
linusehle <linusfinn.ehle@gmail.com>
2024-04-18 17:19:17 +0200

    feat: improved texts and styling

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,16 @@ function About () {
         <div className="pt-16 sm:pt-32 bg-[#000000] text-white">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-3xl lg:text-center">
-                    <h2 className="text-base font-semibold leading-7 text-[#01a7ee]">Über mich</h2>
+                <h2
+        className="font-semibold text-xl"
+        style={{
+          background: "-webkit-linear-gradient(90deg, #9f55ff, #7000ff)",
+          backgroundClip: "text",
+          WebkitTextFillColor: "transparent"
+        }}
+      >
+        Meine Projekte
+      </h2>
                     <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
                         Alles was DU wissen musst
                     </p>
@@ -18,12 +27,21 @@ function About () {
                     <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
                         <div className="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
                             <dt className="flex items-center gap-x-3 text-base font-semibold leading-7">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="h-5 w-5 flex-none text-[#01a7ee]">
-                                    <path d="M2 4.25A2.25 2.25 0 014.25 2h2.5A2.25 2.25 0 019 4.25v2.5A2.25 2.25 0 016.75 9h-2.5A2.25 2.25 0 012 6.75v-2.5zM2 13.25A2.25 2.25 0 014.25
-                                    11h2.5A2.25 2.25 0 019 13.25v2.5A2.25 2.25 0 016.75 18h-2.5A2.25 2.25 0 012 15.75v-2.5zM11 4.25A2.25 2.25 0 0113.25 2h2.5A2.25 2.25 0 0118 4.25v2.5A2.25
-                                    2.25 0 0115.75 9h-2.5A2.25 2.25 0 0111 6.75v-2.5zM15.25 11.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z">
-                                    </path>
-                                </svg>
+                            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+                className="
+                           h-5 w-5 flex-none text-[#7000ff]"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                              01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                  clipRule="evenodd"
+                />
+              </svg>
                                 Web-Design   
                             </dt>
                             <dd className="mt-4 flex flex-auto flex-col text-base leading-7">
@@ -34,12 +52,21 @@ function About () {
                         </div>
                         <div className="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
                             <dt className="flex items-center gap-x-3 text-base font-semibold leading-7">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="h-5 w-5 flex-none text-[#01a7ee]">
-                                    <path d="M2 4.25A2.25 2.25 0 014.25 2h2.5A2.25 2.25 0 019 4.25v2.5A2.25 2.25 0 016.75 9h-2.5A2.25 2.25 0 012 6.75v-2.5zM2 13.25A2.25 2.25 0 014.25
-                                    11h2.5A2.25 2.25 0 019 13.25v2.5A2.25 2.25 0 016.75 18h-2.5A2.25 2.25 0 012 15.75v-2.5zM11 4.25A2.25 2.25 0 0113.25 2h2.5A2.25 2.25 0 0118 4.25v2.5A2.25
-                                    2.25 0 0115.75 9h-2.5A2.25 2.25 0 0111 6.75v-2.5zM15.25 11.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z">
-                                    </path>
-                                </svg>
+                            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+                className="
+                           h-5 w-5 flex-none text-[#7000ff]"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                              01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                  clipRule="evenodd"
+                />
+              </svg>
                                 Suchmaschienenoptimierung
                             </dt>
                             <dd className="mt-4 flex flex-auto flex-col text-base leading-7">
@@ -50,12 +77,21 @@ function About () {
                         </div>
                         <div className="flex flex-col outline outline-1 outline-zinc-800 rounded-lg p-8">
                             <dt className="flex items-center gap-x-3 text-base font-semibold leading-7">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="h-5 w-5 flex-none text-[#01a7ee]">
-                                    <path d="M2 4.25A2.25 2.25 0 014.25 2h2.5A2.25 2.25 0 019 4.25v2.5A2.25 2.25 0 016.75 9h-2.5A2.25 2.25 0 012 6.75v-2.5zM2 13.25A2.25 2.25 0 014.25
-                                    11h2.5A2.25 2.25 0 019 13.25v2.5A2.25 2.25 0 016.75 18h-2.5A2.25 2.25 0 012 15.75v-2.5zM11 4.25A2.25 2.25 0 0113.25 2h2.5A2.25 2.25 0 0118 4.25v2.5A2.25
-                                    2.25 0 0115.75 9h-2.5A2.25 2.25 0 0111 6.75v-2.5zM15.25 11.75a.75.75 0 00-1.5 0v2h-2a.75.75 0 000 1.5h2v2a.75.75 0 001.5 0v-2h2a.75.75 0 000-1.5h-2v-2z">
-                                    </path>
-                                </svg>
+                            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+                className="
+                           h-5 w-5 flex-none text-[#7000ff]"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0
+                              01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+                  clipRule="evenodd"
+                />
+              </svg>
                                 Web-Entwicklung 
                             </dt>
                             <dd className="mt-4 flex flex-auto flex-col text-base leading-7">
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,3 +1,5 @@
+import './projects.scss'
+
 function Projects () {
     return (
         <div
@@ -6,25 +8,30 @@ function Projects () {
 >
   <div className="mx-auto px-4 py-5 sm:px-6 max-w-7xl">
     <div className="max-w-3xl">
-      <h2 className="font-semibold text-[#01a7ee]">Our work</h2>
+      <span
+        className="font-semibold text-xl"
+        style={{
+          background: "-webkit-linear-gradient(90deg, #9f55ff, #7000ff)",
+          backgroundClip: "text",
+          WebkitTextFillColor: "transparent"
+        }}
+      >
+        Meine Projekte
+      </span>
       <p className="mt-2 text-3xl font-bold tracking-tight sm:text-4xl">
         Portfolio
       </p>
-      <p className="mt-4 text-zinc-400">
-        Discover examples of our previous orders and gain an impression of the
-        impression of the quality you can expect from us.
-      </p>
     </div>
-    <div className="mt-10 space-y-16 border-t border-gray-200 pt-10 sm:mt-16">
+    <div className="mt-8 space-y-16 border-t border-gray-200 pt-10 sm:mt-10">
       <a
         className="group mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 lg:flex items-center"
         href=""
       >
         <div>
-          <div className="text-xl font-bold">M&amp;M Handwerk</div>
+          <div className="text-xl font-bold">Projekt Eins</div>
           <div className="text-zinc-400 w-72">
             <div className="mt-2">
-              Website for a craft business with contact form
+              Beschreibung zu Projekt Eins
             </div>
           </div>
         </div>
@@ -34,7 +41,7 @@ function Projects () {
               className="rounded-xl"
               height={700}
               width={225}
-              src="../assets/stonks.png"
+              src="src\assets\stonks.png"
             />
           </div>
           <div className="hidden lg:block">
@@ -42,7 +49,7 @@ function Projects () {
               className="rounded-xl"
               height={700}
               width={225}
-              src="../assets/stonks.png"
+              src="src\assets\stonks.png"
             />
           </div>
           <div className="hidden lg:block">
@@ -50,7 +57,7 @@ function Projects () {
               className="rounded-xl"
               height={700}
               width={225}
-              src="../assets/stonks.png"
+              src="src\assets\stonks.png"
             />
           </div>
         </div>
@@ -60,9 +67,9 @@ function Projects () {
         href=""
       >
         <div>
-          <div className="text-xl font-bold">Ben's Burger</div>
+          <div className="text-xl font-bold">Project Zwei</div>
           <div className="text-zinc-400 w-72">
-            <div className="mt-2">Website for a burger company</div>
+            <div className="mt-2">Beschreibung zu Projekt Zwei</div>
           </div>
         </div>
         <div className="lg:flex lg:space-x-6 space-x-0 overscroll-x-auto pl-16">
@@ -71,7 +78,7 @@ function Projects () {
               className="rounded-xl"
               height={700}
               width={225}
-              src="../assets/stonks.png"
+              src="src\assets\stonks.png"
             />
           </div>
           <div className="hidden lg:block">
@@ -79,7 +86,7 @@ function Projects () {
               className="rounded-xl"
               height={700}
               width={225}
-              src="../assets/stonks.png"
+              src="src\assets\stonks.png"
             />
           </div>
           <div className="hidden lg:block">
@@ -87,7 +94,7 @@ function Projects () {
               className="rounded-xl"
               height={700}
               width={225}
-              src="../assets/stonks.png"
+              src="src\assets\stonks.png"
             />
           </div>
         </div>
