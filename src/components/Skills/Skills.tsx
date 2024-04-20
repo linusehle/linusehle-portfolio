cc3d9580296ba90f8f5dc7fff70a397b23e74cc2
linusehle <linusfinn.ehle@gmail.com>
2024-04-20 16:27:02 +0200

    feat: added section id

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,6 +1,6 @@
 function Skills () {
     return (
-      <div className="bg-[#000000] py-24 sm:py-32 text-white" id="services">
+      <div className="bg-[#000000] py-24 sm:py-32 text-white" id="skills">
   <div className="mx-auto max-w-7xl px-6 lg:px-8">
     <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
       <div>
