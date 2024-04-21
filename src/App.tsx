a9e0d84c5eb3c352cb6113198f4c32a927afc8b2
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2024-04-21 18:28:45 +0200

    feat: changed to current head commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Header from "./components/Header/Header.tsx"
 import About from "./components/About/About.tsx"
-<<<<<<< HEAD
-=======
 import Projects from "./components/Projects/Projects.tsx";
->>>>>>> 1eb217d09f2b16d6884963cb8dbc85cc6e80b930
 import Footer from "./components/Footer/Footer.tsx";
 import Skills from "./components/Skills/Skills.tsx";
 
