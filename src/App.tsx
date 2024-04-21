990ca97e15f372c65021e7a360aee2adb94bba49
Whisper Snowie <104696380+Whisper-Snowie@users.noreply.github.com>
2024-04-21 18:25:09 +0200

    feat: merge to commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Header from "./components/Header/Header.tsx"
 import About from "./components/About/About.tsx"
-import Footer from "./Footer.tsx";
+import Footer from "./components/Footer/Footer.tsx";
 import Skills from "./components/Skills/Skills.tsx";
 
 function App() {
