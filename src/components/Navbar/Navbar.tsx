9bfecbe7e1d51dde23ee17d2b3617d02918f0d0d
linusehle <linusfinn.ehle@gmail.com>
2024-04-23 21:47:44 +0200

    feat(navbar): improved code syntax

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,77 @@
+import { useState } from "react";
+
 function Navbar () {
-    return (
-        <div></div>
-    );
+	const [isOpen, setIsOpen] = useState(false);
+
+	return (
+		<nav className="flex items-center flex-wrap p-6 bg-[#000000] text-white">
+			<div className="block">
+				<button
+				onClick={() => setIsOpen(!isOpen)}
+				className="flex items-center py-2 rounded text-black-500"
+				>
+					<svg
+					className={`fill-current h-5 w-5 ${isOpen ? "block" : "hidden"}`}
+					viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+						d="M0 3h20v2H0V3zm0 
+						6h20v2H0V9zm0 
+						6h20v2H0v-2z"
+						/>
+					</svg>
+					<svg
+					className={`fill-current h-5 w-5 ${isOpen ? "block" : "hidden"}`}
+					viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+					>
+						<path 
+						d="M10 8.586L2.929 1.515 
+						1.515 2.929 8.586 10l-7.071 
+						7.071 1.414 1.414L10 11.414l7.071 
+						7.071 1.414-1.414L11.414 
+						10l7.071-7.071-1.414-1.414L10 8.586z"
+						/>
+					</svg>
+				</button>
+			</div>
+			<div className={`w-full block ${isOpen ? "block" : "hidden"}`}>
+				<div>
+					<a
+					href=""
+					className="block mt-4 text-zinc-400 mr-4"
+					>
+						Start
+					</a>
+					<a
+					href=""
+					className="block mt-4 text-zinc-400 mr-4"
+					>
+						Über mich
+					</a>
+					<a 
+					href=""
+					className="block mt-4 text-zinc-400 mr-4"
+					>
+						Kompetenzen
+					</a>
+					<a
+					href=""
+					className="block mt-4 text-zinc-400 mr-4"
+					>
+						Portfolio
+					</a>
+					<a 
+					href=""
+					className="block mt-4 text-zinc-400 mr-4"
+					>
+						Kontakt
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar
