19380fdabb44e42af06aff1c1a63935ee35640b0
linusehle <linusfinn.ehle@gmail.com>
2024-04-28 10:16:42 +0200

    feat(App.tsx): changed import paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
-import Navbar from "./components/Navbar/Navbar.tsx";
-import Header from "./components/Header/Header.tsx"
-import About from "./components/About/About.tsx"
-import Projects from "./components/Projects/Projects.tsx";
-import Footer from "./components/Footer/Footer.tsx";
-import Skills from "./components/Skills/Skills.tsx";
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar.tsx';
+import Header from './components/Header/Header.tsx';
+import About from './components/About/About.tsx';
+import Projects from './components/Projects/Projects.tsx';
+import Footer from './components/Footer/Footer.tsx';
+import Skills from './components/Skills/Skills.tsx';
+import Imprint from './components/Imprint/Imprint.tsx';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Header />
-      <About />
-      <Skills />
-      <Projects />
-      <Footer />
+      <Routes>
+        <Route 
+        path="/" 
+        element={<Home />} />
+        <Route 
+        path="/impressum" 
+        element={<Imprint />} />
+      </Routes>
+      <Footer/>
     </>
   );
 }
 
-export default App
+function Home() {
+  return (
+    <>
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+    </>
+  );
+}
+
+export default App;
